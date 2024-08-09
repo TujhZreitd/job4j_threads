342aec548c15d08bcad50e2e8be0b5ce58e1a0c4
TujhZreitd <egor.yakushev.96@bk.ru>
2024-08-09 18:28:50 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (char ch : process) {
+                System.out.print("\r load: " + ch);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
+
