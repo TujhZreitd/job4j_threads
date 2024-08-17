423c1cfc8012c2931c004c2ad920597023d6f3ee
TujhZreitd <egor.yakushev.96@bk.ru>
2024-08-17 10:37:24 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger(0);
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
